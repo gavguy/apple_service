b36a28d938df986ab6f576ead57ce4eeb102452b
gavguy <gavguy3@gmail.com>
2019-02-07 17:32:14 +0200

    путаница с гетерами сетерами

--- src/main/java/apple/service/core/phone/model/PhoneEntity.java
+++ src/main/java/apple/service/core/phone/model/PhoneEntity.java
@@ -13,33 +13,83 @@ public class PhoneEntity implements Serializable {
     @Id
     @GeneratedValue
     private Long id;
+
     @NotEmpty(message = "Title shouldn't be empty")
     @Size(max = 200)
-    @Column(name="title", length = 200, nullable = false)
-    private String title;
+    @Column(name="owner", length = 200, nullable = false)
+    private String owner;
+
     @NotEmpty
     @Size(min=10, max = 50)
-    @Column(name="isbn", length = 50, nullable = false, unique = true)
-    private String isbn;
+    @Column(name="product", length = 50, nullable = false)
+    private String product;
+
     @NotEmpty
     @Size(max = 200)
-    @Column(name="author", length = 200, nullable = false)
-    private String author;
+    @Column(name="version", length = 200, nullable = false)
+    private String version;
+
+    @NotEmpty
+    @Size(min=1, max=20)
+    @Column(name="ios", length = 20, nullable = false)
+    private String ios;
+
+    @NotEmpty
+    @Size(min=1, max=30)
+    @Column(name="color", length = 30, nullable = false)
+    private String color;
+
+    @NotEmpty
+    @Size(min=5, max=18)
+    @Column(name="cover", length = 18, nullable = false)
+    private String cover;
+
+
+    @Size(min=1, max=50)
+    @Column(name="country", length = 50, nullable = false)
+    private String country;
+
+
+    @NotEmpty
+    @Size(min=8, max=30)
+    @Column(name="serialNr", length = 30, nullable = false)
+    private String serialNr;
+
     @Min(value=1)
     @NotNull
     @Column(name="year", nullable = false)
     private int year;
+
     @Size(max = 1000)
-    @Column(name="description", length = 1000)
-    private String description;
+    @Column(name="information", length = 1000)
+    private String information;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() {        return id;    }
+    public void setId(Long id) {        this.id = id;    }
+    public String getOwner() {       return owner;    }
+    public void setOwner(String owner) {        this.owner = owner;    }
+    public String getProduct() {        return product;    }
+    public void setProduct(String product) {        this.product = product;    }
+    public String getVersion() {        return version;    }
+    public void setVersion(String version) {        this.version = version;    }
+    public String getIos() {        return ios;    }
+    public void setIos(String ios) {        this.ios = ios;    }
+    public String getColor() {        return color;    }
+    public void setColor(String color) {        this.color = color;    }
+    public String getCover() {        return cover;    }
+    public void setCover(String cover) {        this.cover = cover;    }
+    public String getCountry() {        return country;    }
+    public void setCountry(String country) {        this.country = country;    }
+    public String getSerialNr() {        return serialNr;    }
+    public void setSerialNr(String serialNr) {        this.serialNr = serialNr;    }
+    public String getInformation() {        return information;    }
+    public void setInformation(String information) {        this.information = information;    }
+    public int getYear() {        return year; }
+    public void setYear(int year) {        this.year = year;}
+
+
+
 
     public String getTitle() {
         return title;
@@ -49,12 +99,12 @@ public class PhoneEntity implements Serializable {
         this.title = title;
     }
 
-    public String getIsbn() {
-        return isbn;
+    public String getTipe() {
+        return product;
     }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
+    public void setTipe(String tipe) {
+        this.product = tipe;
     }
 
     public String getAuthor() {
@@ -65,13 +115,7 @@ public class PhoneEntity implements Serializable {
         this.author = author;
     }
 
-    public int getYear() {
-        return year;
-    }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
 
     public String getDescription() {
         return description;
--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -2,9 +2,7 @@ insert into users (id, login_name, password_hash, role_name) values (-1, 'user',
 insert into users (id, login_name, password_hash, role_name) values (-2, 'manager', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'MANAGER')
 insert into users (id, login_name, password_hash, role_name) values (-3, 'admin', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'ADMIN')
 
-insert into books (id, title, isbn, author, year) values (-1, 'Book 1', '123456', 'Ivan Petrov', 1996)
-insert into books (id, title, isbn, author, year) values (-2, 'Book 2', '112233', 'Petr Ivanov', 1997)
-insert into books (id, title, isbn, author, year) values (-3, 'Book 3', '220099', 'John Smith', 1998)
-insert into books (id, title, isbn, author, year) values (-4, 'Book 4', '999999', 'Mike Svarovsky', 2005)
+insert into phones (id, owner, product, version, ios, color, cover, county, year) values (-1, 'andreys esakovs', 'смартфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', 1996)
+
 
 insert into reservations (id, user_id, book_id, status, created) values (-1, -1, -1, 'TAKEN', '2019-01-17')

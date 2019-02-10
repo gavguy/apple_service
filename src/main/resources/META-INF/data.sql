6139104fdd2e5a1a43a28d84546c609253305bd9
gavguy <gavguy3@gmail.com>
2019-02-10 12:04:44 +0200

    l

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -2,9 +2,9 @@ insert into users (id, login_name, password_hash, role_name) values (-1, 'user',
 insert into users (id, login_name, password_hash, role_name) values (-2, 'manager', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'MANAGER')
 insert into users (id, login_name, password_hash, role_name) values (-3, 'admin', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'ADMIN')
 
-insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-1, 'andreys esakovs', 'смартфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '314526734635', 1996)
+insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-1, 'andreys esgakovs', 'смартфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '314526734635', 1996)
 insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-2, 'andеотпа окаkovs', 'цщеыр', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '98765456,' 1996)
 insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-3, 'andreys esakovs', 'смадапкурлелукаолртфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '12345678', 1996)
-insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-4, 'andreys esakovs', 'смадлопкрклртфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '987643', 1996)
+insert into phones (id, owner, product, version, ios, color, cover, county, serialNr, year) values (-4, 'andreys esakkovs', 'смадлопкрклртфон', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '987643', 1996)
 
 

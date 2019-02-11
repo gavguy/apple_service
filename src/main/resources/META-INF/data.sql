ccf1ab750d44c6a657bf6557ef98c74bf2e2169a
gavguy <gavguy3@gmail.com>
2019-02-11 23:08:08 +0200

    где этот синтаксис?

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -2,9 +2,9 @@ insert into users (id, login_name, password_hash, role_name) values (-1, 'user',
 insert into users (id, login_name, password_hash, role_name) values (-2, 'manager', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'MANAGER')
 insert into users (id, login_name, password_hash, role_name) values (-3, 'admin', 'PBKDF2WithHmacSHA256:2048:e9vXAAs/amAf2/PT/eVw2UbG6DcdtIZArf0FqMG8ClI=:UTOAQ8LsucZL3nua/RQj3VyDid3KhaSUFc5AaVI8T4A=', 'ADMIN')
 
-insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year) values (-1, 'andrey andrey', 'smartfone', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '314526734635', 1996)
-insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year) values (-2, 'artem artem', 'nokia', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '98765456,' 1996)
-insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year) values (-3, 'igorj igorj', 'ipod', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '12345678', 1996)
-insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year) values (-4, 'ivan ivan', 'wirelees moyse', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '987643', 1996)
+insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year, information) values (-1, 'andrey andrey', 'smartfone', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '314526734635', 1996,'zarabotaj')
+insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year, information) values (-2, 'artem artem', 'nokia', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '98765456,' 1996, 'ti uze')
+insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year, information) values (-3, 'igorj igorj', 'ipod', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '12345678', 1996,'nakonech')
+insert into phones (id, owner, product, version, ios, color, cover, country, serialNr, year, information) values (-4, 'ivan ivan', 'wirelees moyse', '5s', 'ios-12', 'white', 'classic/special Edition', 'chine', '98764300', 1996,'v konce to koncov')
 
 
